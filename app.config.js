3a90e017c22f7eb68b3d0210308348c44ce91946
Iwaki Kishi <iwaki.kishi@peds3.com>
2025-04-02 23:25:36 +0900

    Fix case sensitivity issues and clean up Git cache

---
+++ app.config.js
@@ -0,0 +1,10 @@
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    },
+  };
+};
